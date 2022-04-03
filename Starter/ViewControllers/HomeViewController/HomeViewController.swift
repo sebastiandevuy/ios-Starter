2623bc16e3c90e8ddcced6b8716dc63c5d9b0cb6
sebastiandevuy <sebastiandevuy@gmail.com>
2022-04-02 23:41:06 -0300

    Removed Storyboards

--- Starter/ViewControllers/HomeViewController/HomeViewController.swift
+++ Starter/ViewControllers/HomeViewController/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  Starter
 //
 //  Created by Pablo Gonzalez on 2/4/22.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .yellow
     }
-
-
 }
-
